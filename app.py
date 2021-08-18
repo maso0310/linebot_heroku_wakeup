5bcacc97d1de8b77b847702781d2e38c5d86a68e
maso0310 <balimon@hotmail.com>
2021-08-18 22:57:27 +0800

    'ok'

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from linebot.models import *
 from message import *
 from new import *
 from Function import *
-from mongodb_function import *
+#from mongodb_function import *
 #======這裡是呼叫的檔案內容=====
 
 #======python的函數庫==========
